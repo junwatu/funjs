d1915e9e41a56acae055c81e9c472d0584ad77e5
Equan Pr <equan.p@gmail.com>
2021-05-04 11:05:00 +0700

    update untuk data import

--- Object.js
+++ Object.js
@@ -2,16 +2,20 @@
  * "Prototype Inheritance"
  * 
  * Sebenarnya bukan inheritance seperti pada halnya pada bahasa OOP
- * tetapi "penurunan" properti suatu objek pada JS sebenarnya adalah
- * properti yang terhubung (linked).
+ * tetapi "penurunan" properti suatu objek pada JavaScript sebenarnya hanyalah
+ * properti yang terhubung (linked) dan bukanlah pengkopian properti
  *   
  */
+import { admin, penerima } from "./Dat"
+
 const Transaksi = {
-  pengirim: "admin@sajen.id",
+  pengirim: admin,
 };
 
+/**
+ * Pembuatan Object baru dengan Object.create() berdasar Object Transaksi
+ */
 const SemuaTransaksi = Object.create(Transaksi);
-SemuaTransaksi.penerima = "mov@jump.com";
-
+SemuaTransaksi.penerima = penerima;
 
 export { Transaksi, SemuaTransaksi };
