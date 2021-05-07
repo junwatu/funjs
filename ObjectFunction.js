43e3e47e356b4c3646cfbb6e6eae42ad4f979f25
Equan Pr <equan.p@gmail.com>
2021-05-08 04:47:18 +0700

    refactor

--- ObjectFunction.js
+++ ObjectFunction.js
@@ -1,12 +1,12 @@
+import { pengirim, penerima } from './Dat'
 /**
  * Pembuatan Objek
  * 
  * 2. Melalui Function (Fungsi)
  * 
  * Meskipun pembuatan objek secara literal (lihat Object.js) banyak dipakai di pengembangan aplikasi JavaScript
- * tetapi untuk pembuatan banyak objek serupa maka pembuatan objek dengan memakai function akan jauh 
+ * tetapi untuk pembuatan banyak objek yang serupa maka pembuatan objek dengan memakai function akan jauh 
  * lebih efektif.
- * 
  *  
  * 
  * Object.getPrototypeOf(Transaksi.prototype) === Object.prototype // true
@@ -16,17 +16,18 @@ const Transaksi = function (pengirim, penerima) {
     this.penerima = penerima
 }
 
+
 /**
- * Instance hashTransaksi tidak lagi mempunyai properti prototype tertulis seperti fungsi Transaksi
- * tetapi hashTransaksi tetap mempunyai properti [[Prototype]].
+ * INSTANCE tx tidak lagi mempunyai properti prototype tertulis seperti fungsi Transaksi
+ * tetapi tx tetap mempunyai properti [[Prototype]].
  * 
- * Object.getPrototypeOf(hashTransaksi0) === Transaksi.prototype // true
+ * Object.getPrototypeOf(tx) === Transaksi.prototype // true
  * 
  * Notasi rantai differential inheritance bisa dituliskan seperti berikut
  * 
- * hashTransaksi.[[Prototype]] → Transaksi.prototype → Object.prototype → null
+ * tx.[[Prototype]] → Transaksi.prototype → Object.prototype → null
  * 
  */
-const hashTransaksi = new Transaksi()
+const tx = new Transaksi(pengirim, penerima)
 
-export { Transaksi as Fx, hashTransaksi as xF }
+export { Transaksi as Fx, tx as xF }
